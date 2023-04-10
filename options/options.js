e7fbaee1917204183fa86521f822aa3044a81e5b
Bradley Lynch <bradleylynch@hillinsurancellc.com>
2023-04-10 14:02:53 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -2,7 +2,7 @@ export const globalOptions = {
   "name": "insurance-app-39775",
   "url": "https://insurance-app-39775.botics.co",
   "api": "https://insurance-app-39775.botics.co/api/v1",
-  "initialRoute": "Untitled1"
+  "initialRoute": "AboutTheAppScreen3"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
