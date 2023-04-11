55cfe213b06d7f9b43e4569f7e530b888854d89c
Bradley Lynch <bradleylynch@hillinsurancellc.com>
2023-04-11 08:28:34 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/AboutTheAppScreen3/index.js
+++ screens/AboutTheAppScreen3/index.js
@@ -1,11 +1,7 @@
 import { StyleSheet } from "react-native";
 import React from "react";
 import Iframe from 'react-iframe';
-
-const App = () => {
-  return <Iframe url="https://cdn.quoteandapply.io/widget.js" width="640px" height="320px" id="" className="" display="block" position="relative" />;
-};
-
+<Iframe url="https://app.back9ins.com/apply/BradleyLynch" width="640px" height="320px" id="" className="" display="block" position="relative" />;
 export default App;
 
 const _styles = StyleSheet.create({
