d234eaac39455e8487ee3e6c187d841361bf2124
Bradley Lynch <bradleylynch@hillinsurancellc.com>
2023-04-11 09:36:46 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/AboutTheAppScreen3/index.js
+++ screens/AboutTheAppScreen3/index.js
@@ -1,11 +1,8 @@
-import { StyleSheet } from "react-native";
-import React from "react";
-import Iframe from 'react-iframe';
-<Iframe url="https://app.back9ins.com/apply/BradleyLynch" width="640px" height="320px" id="" className="" display="block" position="relative" />;
-export default App;
+import React from 'react';
+import { WebView } from 'react-native';
 
-const _styles = StyleSheet.create({
-  kjcDDKAD: {
-    marginTop: 40
-  }
-});
+const Home = () => <WebView source={{
+  uri: 'https://app.back9ins.com/apply/BradleyLynch'
+}} />;
+
+export default Home;
